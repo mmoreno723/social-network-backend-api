526736143965a698634f4b0c30f69680ad78c825
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-07-07 21:55:31 -0400

    reaction model added

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const date = require("date-and-time");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (time) => date.format(time, "MM/DD/YYYY at HH:mm:ss"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
