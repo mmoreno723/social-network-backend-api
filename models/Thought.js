239cdbeb996880a260186e5430027ebfdae11f51
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-07-07 21:48:30 -0400

    thought model added

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const date = require("date-and-time");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (time) => date.format(time, "MM/DD/YYYY at HH:mm:ss"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
